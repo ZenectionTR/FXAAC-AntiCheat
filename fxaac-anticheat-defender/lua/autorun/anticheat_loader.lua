407dc9a914704cd513980a27311decb0bc59cb35
ZenectionTR <zenectionofficial@gmail.com>
2024-05-29 17:58:51 +0300

    XAAC-AntiCheat

--- fxaac-anticheat-defender/lua/autorun/anticheat_loader.lua
+++
@@ -1,6 +0,0 @@
-if SERVER then
-	AddCSLuaFile( "anticheat_main.lua" )
-	AddCSLuaFile( "fxaac/modules/speedhack.lua" )
-	AddCSLuaFile( "fxaac/modules/aimbot.lua" )
-	AddCSLuaFile( "fxaac/modules/initialize.lua" )
-end
--- fxaac-anticheat-defender/lua/fxaac/modules/custom.lua
+++
@@ -1,9 +0,0 @@
--- modules/custom.lua
-local custom = {}
-
--- Example custom function
-function custom.customFunction()
-    print("[Custom] Custom function executed.")
-end
-
-return custom
